192ef496ff2d464ea7d6bcdf4c7a571d8a8a680c
Anto I Lonappan <antoidicherian@gmail.com>
2019-04-10 22:59:14 +0200

    Initial commit

---
+++ tests/test_app.py
@@ -0,0 +1,31 @@
+import unittest
+from cosmopass.likelihoods.chisqaures import model
+
+
+class AppTests(unittest.TestCase):
+    def test_app(self):
+        lcdm = model('LCDM')
+        lcdm.chi_list = ['h','bao']
+
+        bao = lcdm.chi_bao(.3,147,.7,5,.8)
+        h = lcdm.chi_h(.3,147,.7,5,.8)
+        total =  lcdm.chi_sq(.3,147,.7,5,.8)
+        self.assertEqual(total, h+bao)
+#        self.assertNotEqual(squares(2), 5)
+
+#    def test_errors(self):
+#        """Check that method fails when parameter type is not numeric"""
+#        with self.assertRaises(TypeError):
+#            squares("foo")
+
+
+def suite():
+    _suite = unittest.TestSuite()
+    _suite.addTest(AppTests('test_app'))
+    return _suite
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner()
+    runner.run(suite())
+
